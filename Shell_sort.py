a8ad66fc5f8694c96b2a0d5833b4445c73af1e77
Matsu <76900768+IIMatsuII@users.noreply.github.com>
2021-12-13 09:18:38 +0500

    Add files via upload

---
+++ Shell_sort.py
@@ -0,0 +1,10 @@
+def shell(data):
+    inc = len(data) // 2
+    while inc:
+        for i, el, in enumerate(data):
+            while i >= inc and data[i - inc] > el:
+                data[i] = data[i - inc]
+                i -= inc
+            data[i] = el
+        inc = 1 if inc == 2 else int(inc * 5.0 / 11)
+    return data
