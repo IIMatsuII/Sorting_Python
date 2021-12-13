69cf736fef8c2e070cf2b02ddb5b05e683bef5ac
Matsu <76900768+IIMatsuII@users.noreply.github.com>
2021-12-13 09:06:07 +0500

    Add files via upload

---
+++ Insertion_sort.py
@@ -0,0 +1,14 @@
+def insertion_sort(alist): #Функция принимающая на вход список
+    for i in range(1, len(alist)): #цикл с переменой i до длины списка - 1
+        temp = alist[i] #Создаем переменую
+        j = i - 1
+        while (j >= 0 and temp < alist[j]): #Создадим цикл который будет работать до тех пор пока j не отрицательное и temp меньше чем элемент под индексом j
+            alist[j + 1] = alist[j]
+            j -= 1
+        alist[j + 1] = temp
+
+alist = input("Enter the list of numbers: ").split()
+alist = [int(x) for x in alist]
+insertion_sort(alist)
+print("sorted list: ", end="")
+print(alist)
