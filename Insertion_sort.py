97c5252ab0fd285f74d90c0af0b86546ba861b80
Matsu <76900768+IIMatsuII@users.noreply.github.com>
2021-12-13 09:14:03 +0500

    Add files via upload

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -12,3 +12,15 @@ alist = [int(x) for x in alist]
 insertion_sort(alist)
 print("sorted list: ", end="")
 print(alist)
+
+"""
+def insertion(data):
+    for i in range(len(data))
+        j = i + 1
+        key = data[i]
+        while data[j] > key and j >= 0:
+            data[j + 1] = data[j]
+            j -= 1
+            data[j + 1] = key
+    return data
+"""
